b0cb6923181bc941b88cd672eb78a10e956b467c
emirali101 <147541566+emirali101@users.noreply.github.com>
2023-10-22 11:18:20 +0200

    Update forms.py

--- forms.py
+++ forms.py
@@ -3,13 +3,14 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email
 
 
+
 class ContactForm(FlaskForm):
-    name = StringField("Name", render_kw={'placeholder': 'Name'},
+    name = StringField("Name", render_kw={'placeholder': 'Name*'},
                        validators=[DataRequired("Please enter your name.")])
-    email = StringField("Email", render_kw={'placeholder': 'Email'},
+    email = StringField("Email", render_kw={'placeholder': 'Email*'},
                         validators=[DataRequired("Please enter your email address."), Email()])
-    subject = StringField("Subject", render_kw={'placeholder': 'Subject'},
+    subject = StringField("Subject", render_kw={'placeholder': 'Subject*'},
                           validators=[DataRequired("Please enter a subject.")])
-    message = TextAreaField("Message", render_kw={'placeholder': 'Message'},
+    message = TextAreaField("Message", render_kw={'placeholder': 'Message*'},
                             validators=[DataRequired("Please enter a message.")])
     submit = SubmitField("Send")
